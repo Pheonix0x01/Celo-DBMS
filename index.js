90062a4b9d17013a1424841ccb426dee5c4ca0df
Phoenix <139072869+Pheonix0x01@users.noreply.github.com>
2023-09-29 10:28:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let path = require("path");
 let webpack = require("webpack");
 let webpackDevServer = require("webpack-dev-server");
-let webpackConfig = require("./webpack.config");
+let webpackConfig = require("webpack.config");
 
 let webpackDevServerOptions = {
   publicPath: "/",
